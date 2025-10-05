33f1e6a9e05facfbbdd3c42f9f7db1b1fddfe816
johndelrosario-create <john.delrosario@oed.com.ph>
2025-10-05 18:28:58 +0800

    chore: upgrade Android SDK to 36, Kotlin to 2.1.0, and Java to 17

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,20 +7,20 @@ plugins {
 
 android {
     namespace = "io.intelliboro.intelliboro"
-    compileSdk = 35
+    compileSdk = 36
     //ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
 
          // For plugin: flutter_local_notifications
         isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
@@ -28,7 +28,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
